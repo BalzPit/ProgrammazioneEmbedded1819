1490cf8619bd368163f9c21eb1587fa8d53c41f2
Pietro <pietro.balzan@studenti.unipd.it>
2019-06-03 15:43:05 +0200

    ready to merge
    prepared code to be merged with daniele's additions

--- app/src/main/java/gruppoembedded/pse1819/unipd/project/MainActivity.java
+++ app/src/main/java/gruppoembedded/pse1819/unipd/project/MainActivity.java
@@ -1,12 +1,7 @@
 package gruppoembedded.pse1819.unipd.project;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
-import gruppoembedded.pse1819.unipd.project.tensorflowlite.ClassifierActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,15 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button epicButton = findViewById(R.id.button);
-
-        epicButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), ClassifierActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
